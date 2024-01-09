cd64c334939b74aa80724499061af76a087da345
AlexanderTheWise <aguillenhernandez@hotmail.com>
2024-01-09 01:09:48 +0100

    Add create, delete, toggle subtask cases

--- src/data-access/task.db.ts
+++ src/data-access/task.db.ts
@@ -2,7 +2,7 @@ import { type UpdateQuery } from "mongoose";
 import { type ITask, type IModels } from "../models";
 
 export const makeTaskDb = (Task: IModels["Task"]) => {
-  const insert = async (task: ITask) => Task.create(task);
+  const insert = async (task: Omit<ITask, "subtasks">) => Task.create(task);
 
   const update = async (_id: string, updateQuery: UpdateQuery<ITask>) =>
     Task.updateOne({ _id }, updateQuery);
@@ -11,7 +11,13 @@ export const makeTaskDb = (Task: IModels["Task"]) => {
 
   const find = async (_id: string) => Task.findById(_id);
 
-  return Object.freeze({ insert, update, remove, find });
+  const updateSubtask = async (
+    _id: string,
+    subtask_id: string,
+    updateQuery: UpdateQuery<ITask>,
+  ) => Task.updateOne({ _id, "subtasks._id": subtask_id }, updateQuery);
+
+  return Object.freeze({ insert, update, remove, find, updateSubtask });
 };
 
 export type TaskDb = ReturnType<typeof makeTaskDb>;
