75b9f59a01347d216c0f121abb35f52be89013bb
AlexanderTheWise <aguillenhernandez@hotmail.com>
2024-01-19 01:48:54 +0100

    Add board route

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,8 @@ import cors from "cors";
 import morgan from "morgan";
 import compression from "compression";
 import logger from "./logger";
-import userRouter from "./routes/user.route";
+import userRouter, { auth } from "./routes/user.route";
+import boardRouter from "./routes/board.route";
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(morgan("tiny"));
 app.use(express.json());
 app.use(compression());
 app.use("/user", userRouter);
+app.use("/board", auth, boardRouter);
 
 const startServer = (port: number) => {
   const server = app.listen(port, () => {
